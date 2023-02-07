22bb084d0221fac22144d1653738001a5763f348
Irfnmdlptra <bangasrat@gmail.com>
2023-02-07 17:58:06 +0800

    Menambahkan Event Tambah Post

--- latihan 1/react-hellow-world/src/container/BlogPost/BlogPost.jsx
+++ latihan 1/react-hellow-world/src/container/BlogPost/BlogPost.jsx
@@ -6,41 +6,66 @@ import axios, { Axios } from "axios";
 class BlogPost extends Component {
   state = {
     post: [],
+    formBlogPost: {
+      id: 1,
+      title: '',
+      body: '',
+      userId: 1
+    }
   };
+ 
   getPostAPI = () => {
-    axios.get('http://localhost:3004/posts')
-    .then((result) => {
-      this.setState ({
+    axios.get("http://localhost:3004/posts?_sort=id&_order=desc").then((result) => {
+      this.setState({
         post: result.data
-      })
+      });
+    });
+  };
+  postDataToAPI = () => {
+    axios.post('http://localhost:3004/posts', this.state.formBlogPost). then((res) => {
+      console.log(res);
+      this.getPostAPI();
+    }, (err) => {
+      console.log('error: ', err)
     })
   }
   handleRemove = (data) => {
     console.log(data);
-    axios.delete(`http://localhost:3004/${data}`).then((res) => {
-      console.log(res)
+    axios.delete(`http://localhost:3004/posts/${data}`).then((res) => {
+      this.getPostAPI();
+    });
+  };
+
+  handleFormChange = (event) => {
+    let formBlogPostNew = {...this.state.formBlogPost}
+    let timestamp = new Date().getTime()
+    formBlogPostNew['id']= timestamp;
+    formBlogPostNew[event.target.name] = event.target.value;
+    this.setState ({
+      formBlogPost: formBlogPostNew
+    }, () => {
+      console.log('value obj formBlogPost', this.state.formBlogPost)
     })
+  };
+  handleSubmit = () => {
+    this.postDataToAPI();
   }
   componentDidMount() {
-    // fetch("https://jsonplaceholder.typicode.com/posts")
-    //       .then((response) => response.json())
-    //       .then((json) => {
-    //         this.setState({
-    //           post: json,
-    //         });
-    //       });
-    axios.get('http://localhost:3004/posts')
-    .then((result) => {
-      this.setState({
-        post:result.data
-      })
-    })
+    this.getPostAPI();
   }
 
   render() {
     return (
       <Fragment>
-        <p className="section-title">Blog Post</p>
+        <p className="section-title" >Blog Post</p>
+        <button type="submit" className="btn-buat">Buat Blog</button>
+        <div className="form-add-post">
+          <label htmlFor="title">Title</label>
+          <input type="text" name="title" placeholder="tambah title" onChange={this.handleFormChange}/>
+          <label htmlFor="body">Blog Content</label>
+          <textarea name="body" id="body"  cols="30" rows="10" placeholder="add blog content" onChange={this.handleFormChange}></textarea>
+          <button type="submit" className="btn-submit" onClick={this.handleSubmit}>Simpan</button>
+        </div>
         <div className="container">
           {this.state.post.map((post) => {
             return <Post key={post.id} data={post} remove={this.handleRemove}/>;
